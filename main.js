ea1863335606403644946f6107ee8d7436405a08
Hoang_Thai <tlee692001@gmail.com>
2023-04-29 15:36:18 +0700

    review, update and fix
    add Validate:
    - input type= "text"
    - input type="email"
    - input type="password"
    - input type="radio"
    - input type="checkbox"
    - input type="file"
    - select

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 function Validator(options) {
+    function getParent(element, selector) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement;
+            }
+            element = element.parentElement;;
+        }
+    }
+
     var selectorRules = {}
     function validate(inputElement, rule) {
-        var errorElement = inputElement.parentElement.querySelector(options.errorSelector)
+        var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector)
         var errorMessage;
 
         var rules = selectorRules[rule.selector]
 
         for (var i = 0; i < rules.length; i++) {
-            errorMessage = rules[i](inputElement.value)
+            switch (inputElement.type) {
+                case 'radio':
+                case 'checkbox':
+                    errorMessage = rules[i](
+                        document.querySelector(rule.selector + ':checked')
+                    )
+
+                    break;
+                default:
+                    errorMessage = rules[i](inputElement.value)
+            }
             if (errorMessage) break;
         }
         if (errorMessage) {
             errorElement.innerText = errorMessage;
-            inputElement.parentElement.classList.add('invalid')
+            getParent(inputElement, options.formGroupSelector).classList.add('invalid')
         } else {
             errorElement.innerText = '';
-            inputElement.parentElement.classList.remove('invalid')
+            getParent(inputElement, options.formGroupSelector).classList.remove('invalid')
 
         }
         return !errorMessage;
@@ -30,20 +49,50 @@ function Validator(options) {
                 var isValid = validate(inputElement, rule)
                 if (!isValid) {
                     isFormValid = false;
+
                 }
             });
             if (isFormValid) {
                 if (typeof options.onSubmit === 'function') {
                     var enableInputs = formElement.querySelectorAll('[name]:not([disable])')
-                    var formValues = Array.from(enableInputs).reduce(function (value, input) {
-                        return (value[input.name] = input.value) && value;
+                    var formValues = Array.from(enableInputs).reduce(function (values, input) {
+                        switch (input.type) {
+                            case 'radio':
+                                if (input.checked) {
+                                    values[input.name] = input.value
+                                }
+                                if (!values[input.name]) {
+                                    values[input.name] = ''
+                                }
+                                break;
+                            case 'checkbox':
+                                if (input.checked) {
+                                    if (Array.isArray(values[input.name])) {
+                                        values[input.name].push(input.value)
+                                    } else {
+                                        values[input.name] = [input.value]
+                                    }
+                                }
+                                if (!values[input.name]) {
+                                    values[input.name] = ''
+                                }
+                                break;
+
+                            case 'file':
+                                values[input.name] = input.files
+                                break;
+                            default:
+                                values[input.name] = input.value;
+                        }
+                        return values;
                     }, {})
                     options.onSubmit(formValues);
-                }
-            } else {
-                formElement.submit();
-            }
 
+                }
+                else {
+                    formElement.submit()
+                }
+            }
         }
         options.rules.forEach(function (rule) {
 
@@ -52,18 +101,24 @@ function Validator(options) {
             } else {
                 selectorRules[rule.selector] = [rule.test]
             }
-            var inputElement = formElement.querySelector(rule.selector)
-
-            if (inputElement) {
+            var inputElements = formElement.querySelectorAll(rule.selector)
+            Array.from(inputElements).forEach(function (inputElement) {
+                var parentElementInput = getParent(inputElement, options.formGroupSelector);
                 inputElement.onblur = function () {
                     validate(inputElement, rule)
                 }
                 inputElement.oninput = function () {
-                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector)
+                    var errorElement = parentElementInput.querySelector(options.errorSelector)
                     errorElement.innerText = '';
-                    inputElement.parentElement.classList.remove('invalid')
+                    parentElementInput.classList.remove('invalid')
                 }
-            }
+                inputElement.onchange = function () {
+                    var errorElement = parentElementInput.querySelector(options.errorSelector)
+                    errorElement.innerText = '';
+                    parentElementInput.classList.remove('invalid')
+                }
+            })
+
         })
     }
 }
@@ -72,7 +127,7 @@ Validator.isRequired = function (selector, message) {
     return {
         selector: selector,
         test: function (value) {
-            return value.trim() ? undefined : message || 'Vui lòng nhập trường này.'
+            return value ? undefined : message || 'Vui lòng nhập trường này.'
         }
     }
 
